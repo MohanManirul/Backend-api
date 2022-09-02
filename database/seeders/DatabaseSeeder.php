ebfa4f62e5b7205f9c5654fc3166e52b336cfee4
MohanManirul <fiforeg@gmail.com>
2022-09-02 19:58:47 +0600

    resource

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            BlogSeeder::class
+            BlogSeeder::class,
+            UserSeeder::class
         ]);
     }
 }
